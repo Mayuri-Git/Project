641ed0923dd510cadd1c93056590fb71150688dd
Mayuri <mayuri@users.noreply.github.com>
2018-04-04 10:30:55 -0700

    experimentation with instruments

--- musicLang/src/musicLang/Sample1.java
+++ musicLang/src/musicLang/Sample1.java
@@ -33,15 +33,48 @@ public class Sample1 {
 			Instrument[] instr = midiSynth.getDefaultSoundbank().getInstruments();
 			final MidiChannel[] mChannels = midiSynth.getChannels();
 
-			mChannels[0].programChange(50);	// program change parameter changes instrument
+			mChannels[0].programChange(0);	// program change parameter changes instrument
 
 
-			mChannels[0].noteOn(50, 100);//On channel 0, play note number 60 with velocity 100
-			try { Thread.sleep(2000); // wait time in milliseconds to control duration
+			mChannels[0].noteOn(60, 100);//On channel 0, play note number 60 with velocity 100
+			try { Thread.sleep(5000); // wait time in milliseconds to control duration
 			} catch( InterruptedException e ) { }
 			mChannels[0].noteOff(60);//turn of the note
-
-
+			
+			
+			
+			
+			//analyzing different instruments
+			
+			
+			Instrument[] instruments = midiSynth.getDefaultSoundbank().getInstruments();
+			final MidiChannel[] channels = midiSynth.getChannels();
+			
+			Synthesizer synth = MidiSystem.getSynthesizer();
+			synth.open();
+			
+			//steel drums
+			int instrument = 114;
+			int velocity = 100;
+			int note = 60;
+			
+			synth.loadInstrument(instruments[instrument]);
+			channels[0].programChange(instrument);
+			
+			System.out.println(instruments[instrument].getName());
+			
+			mChannels[0].noteOn(note, velocity);
+			try { Thread.sleep(5000); // wait time in milliseconds to control duration
+			} catch( InterruptedException e ) { }
+			mChannels[0].noteOff(note);//turn of the note
+			
+			
+			//loading all instruments
+			for(Instrument instrument1 : instr) {
+				System.out.println(instrument1);
+			}
+			
+			
 		} catch (MidiUnavailableException e) {}
 	}
 
